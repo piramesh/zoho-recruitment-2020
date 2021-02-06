b416e529d51f01d5fd589c831bdad51f342843c9
piramesh <78610324+piramesh@users.noreply.github.com>
2021-02-06 09:59:36 +0530

    Create piramesh.java

---
+++ piramesh.java
@@ -0,0 +1,21 @@
+public class piramesh {
+   public static void main(String []args) {
+	int n=5;
+	for(int i=n;i>=1;i--) {
+		for(int j=1;j<=n;j++) {
+			if(j<i)
+			{
+				System.out.print(i);
+			}
+			else {
+				System.out.print(j);
+			}
+		}
+		System .out.println("");
+	}
+	
+		
+	
+		}
+}
+	
